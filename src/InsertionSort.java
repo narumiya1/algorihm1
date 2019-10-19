786290163e550a3bbf1fd4db9c749ea71408d1c2
narumiya1 <narumiiyamei@gmail.com>
2019-10-19 19:46:00 +0700

    Insertion Sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,34 @@
+
+public class InsertionSort {
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+
+            int key = array[i];
+
+            int j = i-1;
+            while ( (j > -1) && ( array [j] > key ) ) {
+                array [j+1] = array [j];
+                j= j-1;
+            }
+
+            array[j+1] = key;
+        }
+    }
+
+    public static void main(String a[]){
+        int[] arr1 = {97,25,67,46,10,7,4};
+        System.out.println("Before Insertion Sort");
+        for(int i:arr1){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        insertionSort(arr1);//sorting array using insertion sort    
+
+        System.out.println("After Insertion Sort");
+        for(int i:arr1){
+            System.out.print(i+" ");
+        }
+    }
+}
